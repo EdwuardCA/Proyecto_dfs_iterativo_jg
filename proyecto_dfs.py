04fc1f4f81fff8420f992ca0ce3f2bf2d1f63eb3
EdwuardCA <luis.chay@tecdesoftware.edu.mx>
2026-02-26 23:34:58 -0600

    Commit de estructura Stack

--- proyecto_dfs.py
+++ proyecto_dfs.py
@@ -1,5 +1,28 @@
 class Stack:
-    pass
+    #Implementacion de una pila con lista
+    def __init__(self):
+        self.data = []
+
+    def push(self, x):
+        self.data.append(x)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.data.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.data[-1]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def size(self):
+        return len(self.data)
+    
+
 
 class Graph:
     pass
